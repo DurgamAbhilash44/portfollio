2aa05bca1fbb740753f719bd43818aecab938014
DurgamAbhilash44 <durgamabhilash798@gmail.com>
2024-09-30 18:14:59 +0530

    Your commit message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
-import Nav from '../assets/logo.png';
+import Nav from '../assets/image.png';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
